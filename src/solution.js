1309d82f744e2fce0e305fb6b14b7e98f7a6b6dd
Benji Oxner <boxner5107@gmail.com>
2022-10-13 17:16:19 -0400

    changed chooseItemByNameAndSize

--- src/solution.js
+++ src/solution.js
@@ -11,63 +11,43 @@
    
   When any of the following function's parameters reference `product`, they are referencing an object with the above shape.
 */
-
+let products = [
+	{
+		name: "Court Sneaker",
+		priceInCents: 9800,
+		availableSizes: [5.5, 8, 8.5, 9, 9.5, 10, 10.5, 11],
+	},
+	{
+		name: "Relaxed Silk Shirt",
+		priceInCents: 9800,
+		availableSizes: [0, 10, 12, 2],
+	},
+	{
+		name: "Square-Neck Jumpsuit",
+		priceInCents: 8800,
+		availableSizes: [6, 10, 14, 2, 12],
+	},
+];
 function printablePrice(priceInCents) {
-  const amount = (priceInCents / 100).toFixed(2);
-  return `$${amount}`;
+	const amount = (priceInCents / 100).toFixed(2);
+	return `$${amount}`;
 }
-
-function chooseItemByNameAndSize(products ={},name = null,size = null) {
-  for (const selectedProduct in products) {
-      const product = products[selectedProduct];
-      if (product.name === name) {
-        if (product.availableSizes.includes(size)){
-          return product
-        }
-      }
-  }
-    return null;
-}  
-
-function addProductToCart(product,cart = {}) {
-  let {name,priceInCents} = product;
-  if (!cart[name]){
-    const productInfo = {priceInCents, quantity: 1}
-    cart[name] = productInfo;
-  } else {
-    cart[name].quantity++// or you could have done cart[name].quantity = cart[name].quantity + 1
-  }
-  return cart
+function chooseItemByNameAndSize(products, name, size) {
+	let result = products.find((productObj) => {
+		if (productObj.name === name && productObj.availableSizes.includes(size)) {
+			return productObj;
+		}
+	});
+	return result ? result : null;
 }
+function addProductToCart(product, cart = {}) {}
+function calculateTotal(cart) {}
 
-
-
-function calculateTotal(cart) {
-  let total = 0;
-  for (const currentItem in cart) {
-    const item = cart[currentItem];
-    total += (item.priceInCents * item.quantity);  
-    }
-    return total;
-  }
-
-
-  function printReceipt(cart) {
-    const total = calculateTotal(cart);
-    if (!total) return null;
-  
-    let result = "";
-    for (let currentItem in cart) {
-        const {quantity,priceInCents} = cart[currentItem];
-        const amount = printablePrice(quantity*priceInCents);
-        result += `${quantity}x${currentItem} - ${amount}\n`;
-      }
-      return result + `Total: ${printablePrice(total)}`;
-    }
+function printReceipt(cart) {}
 
 module.exports = {
-  chooseItemByNameAndSize,
-  addProductToCart,
-  calculateTotal,
-  printReceipt,
-}
+	chooseItemByNameAndSize,
+	addProductToCart,
+	calculateTotal,
+	printReceipt,
+};
